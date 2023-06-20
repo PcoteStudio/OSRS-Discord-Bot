0c87586b77300a2b65881beefeb5f763dfdab985
Philippe Côté <cote.philippe@hotmail.com>
2023-06-20 16:58:49 -0400

    Added a help command.

--- src/cogs/developercog.py
+++ src/cogs/developercog.py
@@ -1,4 +1,6 @@
+import nextcord
 from nextcord.ext import commands
+from internal import constants
 
 
 class DeveloperCog(commands.Cog):
@@ -7,6 +9,23 @@ class DeveloperCog(commands.Cog):
     @commands.is_owner()
     async def ping(self, ctx):
         await ctx.send("Pong")
+
+    @nextcord.slash_command(guild_ids=constants.COMMANDS_GUILD_ID)
+    async def help(self, interaction):
+        content = "**Available commands**"
+        content += "```"
+        content += "\nGame commands"
+        content += "\n  /stats        \tDisplays every team's statistics"
+        content += "\n  /stats <user> \tDisplays the player's team statistics"
+        content += "\n  /team list    \tDisplays a list of every team and their members"
+        content += "\nTeam commands"
+        content += "\n  /current    \tDisplay the team's current grind"
+        content += "\n  /roll       \tRoll the team's next grind"
+        content += "\n  /team name  \tSets your team's name"
+        content += "\n  /team emoji \tSets your team's emoji"
+        content += "\n  /team color \tSets your team's color on the board"
+        content += "\n```"
+        await interaction.send(content)
 
 
 def setup(bot):
