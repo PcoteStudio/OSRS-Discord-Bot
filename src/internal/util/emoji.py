aca5defc097ce31f989b6adc2266261966feb85b
Nicolas Signori <n.signo@hotmail.com>
2023-07-05 17:47:19 -0400

    Save new emojis to database 😁

--- src/internal/util/emoji.py
+++ src/internal/util/emoji.py
@@ -5,6 +5,32 @@ import aiohttp
 import aiofiles
 
 
+class Emoji:
+    def __init__(self, emoji_string: str, emoji_id: str = None, animated: bool = None):
+        self.emoji_string = emoji_string
+        self.emoji_id = emoji_id
+        self.animated = animated
+
+    @classmethod
+    async def load_emoji(cls, emoji_string):
+        if not emoji_string.startswith('<'):
+            return cls(emoji_string)
+
+        parsed_emoji_string = re.search(r"<(a?)(:\w*:)(\d*)>", emoji_string)
+        emoji_id = parsed_emoji_string.group(3)
+
+        emoji = await find_emoji(emoji_id)
+        if not emoji:
+            animated = True if parsed_emoji_string.group(1) else False
+            emoji_string = parsed_emoji_string.group(2)
+            emoji = await create_emoji(emoji_string, emoji_id, animated)
+
+        return emoji
+
+
+from database import emojidb
+
+
 def build_url(emoji_id, image_extension):
     # 'https://cdn.discordapp.com/emojis/1062676457341599775.webp?size=96&quality=lossless'
     base = 'https://cdn.discordapp.com/emojis/'
@@ -29,23 +55,15 @@ async def fetch_image(emoji_id, animated):
             async with aiofiles.open(path_image, "+wb") as f:
                 await f.write(content)
 
-class Emoji:
-    def __init__(self, emoji_string: str):
-        if not emoji_string.startswith('<'):
-            self.is_custom = False
-            self.emoji_id = emoji_string
-            return
 
-        self.is_custom = True
+async def find_emoji(emoji_id):
+    emoji = await emojidb.get_emoji_by_id(emoji_id)
+    return emoji
 
-        result = re.search(r"<(a?)(:\w*:)(\d*)>", emoji_string)
 
-        self.animated = True if result.group(1) else False
-        self.emoji_string = result.group(2)
-        self.emoji_id = result.group(3)
+async def create_emoji(emoji_string, emoji_id, animated):
+    emoji = Emoji(emoji_string, emoji_id, animated)
+    await fetch_image(emoji.emoji_id, emoji.animated)
+    await emojidb.insert(emoji)
 
-    @classmethod
-    async def create_emoji(cls, emoji_string):
-        self = cls(emoji_string)
-
-        await fetch_image(self.emoji_id, self.animated)
+    return emoji
